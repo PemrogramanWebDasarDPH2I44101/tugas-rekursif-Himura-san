51f3d0d8cd7e2afd2dd3d96b576b18028e13e257
Zahid Musthofainal Akhyar <32158770+Himura-san@users.noreply.github.com>
2018-09-09 12:13:19 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,44 @@
+<form method="post">
+    Angka : 
+    <input type="number" name="angka">
+    <input type="submit" value="Submit">
+</form>
 <?php
+  $angka = $_POST['angka'];
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+      function rekursif_piramid($i, $j){
+          if ($i > 0) {
+              if ($j > 0) {
+                  echo "* ";
+                  $j--;
+                  rekursif_piramid($i, $j);
+              }
+              else {
+                  echo "<br>";
+                  $i--;
+                  rekursif_piramid($i, $i);
+              }
+          }
+      }
+      rekursif_piramid($angka, $angka);
+
+  // ================================================================================== //
+      echo "<br>";
+  // ================================================================================== //
+
+      function rekursif_angka($i, $j, $angka){
+          if ($i <= $angka) {
+              if ($j <= $i) {
+                  echo "$j ";
+                  $j++;
+                  rekursif_angka($i, $j, $angka);
+              }
+              else {
+                  echo "<br>";
+                  $i++;
+                  rekursif_angka($i, 1, $angka);
+              }
+          }
+      }
+      rekursif_angka(1, 1, $angka);
 ?>
